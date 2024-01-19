973eb4652e515c5a00399a3bab39577844e0a2fa
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-01-19 19:53:08 +0530

    -test

---
+++ Codes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String name ="Ash";
+        int age=19;
+        String newname="hehe";
+        String Duplicate=name;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(newname);
+        System.out.println(Duplicate);
+    }
+}
